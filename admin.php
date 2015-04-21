e87a6e941a9ec15c45aa32ea0564647a5a7f72ba
akshatc <akshatc@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-21 10:29:28 +0000

    Removing incompatibility between pantheon plugin and blogvault plugin.
    git-svn-id: https://plugins.svn.wordpress.org/bv-pantheon-migration/trunk@1140455 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin.php
+++ admin.php
@@ -42,11 +42,7 @@ if (!function_exists('bvPantheonAdminInitHandler')) :
 
 		if ($blogvault->getOption('bvActivateRedirect') === 'yes') {
 			$blogvault->updateOption('bvActivateRedirect', 'no');
-			if (defined('BVMIGRATEPLUGIN')) {
-				wp_redirect('admin.php?page=bv-pantheon-migrate');
-			} else {
-				wp_redirect('admin.php?page=bv-pantheon-key-config');
-			}
+			wp_redirect('admin.php?page=bv-pantheon-migrate');
 		}
 	}
 	add_action('admin_init', 'bvPantheonAdminInitHandler');
@@ -54,10 +50,7 @@ endif;
 
 if (!function_exists('bvPantheonAdminMenu')) :
 	function bvPantheonAdminMenu() {
-		add_menu_page('bV Pantheon', 'bV Pantheon', 'manage_options', 'bv-pantheon-key-config', 'bvKeyConf');
-		if (defined('BVMIGRATEPLUGIN')) {
-			add_submenu_page('bv-pantheon-key-config', 'blogVault', 'Migrate Site', 'manage_options', 'bv-pantheon-migrate', 'bvMigrate');
-		}
+		add_menu_page('bV Pantheon', 'bV Pantheon', 'manage_options', 'bv-pantheon-migrate', 'bvPantheonMigrate');
 	}
 	add_action('admin_menu', 'bvPantheonAdminMenu');
 endif;
@@ -65,15 +58,15 @@ endif;
 if ( !function_exists('bvSettingsLink') ) :
 	function bvSettingsLink($links, $file) {
 		if ( $file == plugin_basename( dirname(__FILE__).'/blogvault.php' ) ) {
-			$links[] = '<a href="' . admin_url( 'admin.php?page=bv-pantheon-key-config' ) . '">'.__( 'Settings' ).'</a>';
+			$links[] = '<a href="' . admin_url( 'admin.php?page=bv-pantheon-migrate' ) . '">'.__( 'Settings' ).'</a>';
 		}
 		return $links;
 	}
 	add_filter('plugin_action_links', 'bvSettingsLink', 10, 2);
 endif;
 
-if ( !function_exists('bvMigrate') ) :
-	function bvMigrate() {
+if ( !function_exists('bvPantheonMigrate') ) :
+	function bvPantheonMigrate() {
 		global $blogvault, $bvNotice;
 		$_error = NULL;
 		if (isset($_GET['error'])) {
@@ -95,26 +88,26 @@ if ( !function_exists('bvMigrate') ) :
 <?php if ($_GET["tutorial"]) {
 	// PUT TUTORIAL HERE
 ?>
-<h1>How to get Pantheon SFTP Credentials</h1>
-<p>blogVault requires SFTP credentials to copy files from your current site to the destination Pantheon site. This information can easily be retrieved from your Pantheon dashboard.<p>
-<ol>
-<li>Login to Pantheon Dashboard and select your site.</li>
-<li>Click on <i>Visit Development Site</i>. This will redirect you to your development site. Copy the url of the site and enter it in <strong><a href="<?php echo admin_url('admin.php?page=bv-pantheon-migrate') ?>">Pantheon URL</a></strong><br/>
-<img src="<?php echo plugins_url('pantheon-url.png', __FILE__); ?>"/>
-</li>
-<li>Click on <i>Connection Info</i> present at top of site status box. In the dropdown look for <strong>SFTP</strong> section.</br>
-		<img src="<?php echo plugins_url('connection_info.png', __FILE__); ?>"/>
-</li>
-<li>
-Copy Host inside SFTP and paste it in <strong><a href="<?php echo admin_url('admin.php?page=bv-pantheon-migrate') ?>">SFTP Server Address</a></strong>
-</li>
-<li>
-Username can be directly copied from Username box under SFTP and pasted in <strong><a href="<?php echo admin_url('admin.php?page=bv-pantheon-migrate') ?>">SFTP Username</a></strong>.
-</li>
-<li>
-As clear from the picture above, password is same as your Pantheon dashboard password. Enter it under <strong><a href="<?php echo admin_url('admin.php?page=bv-pantheon-migrate') ?>">SFTP Password</a></strong>.
-</li>
-</ol>
+	<h1>How to get Pantheon SFTP Credentials</h1>
+	<p>blogVault requires SFTP credentials to copy files from your current site to the destination Pantheon site. This information can easily be retrieved from your Pantheon dashboard.<p>
+	<ol>
+		<li>Login to Pantheon Dashboard and select your site.</li>
+		<li>Click on <i>Visit Development Site</i>. This will redirect you to your development site. Copy the url of the site and enter it in <strong><a href="<?php echo admin_url('admin.php?page=bv-pantheon-migrate') ?>">Pantheon URL</a></strong><br/>
+			<img src="<?php echo plugins_url('pantheon-url.png', __FILE__); ?>"/>
+		</li>
+		<li>Click on <i>Connection Info</i> present at top of site status box. In the dropdown look for <strong>SFTP</strong> section.</br>
+				<img src="<?php echo plugins_url('connection_info.png', __FILE__); ?>"/>
+		</li>
+		<li>
+			Copy Host inside SFTP and paste it in <strong><a href="<?php echo admin_url('admin.php?page=bv-pantheon-migrate') ?>">SFTP Server Address</a></strong>
+		</li>
+		<li>
+			Username can be directly copied from Username box under SFTP and pasted in <strong><a href="<?php echo admin_url('admin.php?page=bv-pantheon-migrate') ?>">SFTP Username</a></strong>.
+		</li>
+		<li>
+			As clear from the picture above, password is same as your Pantheon dashboard password. Enter it under <strong><a href="<?php echo admin_url('admin.php?page=bv-pantheon-migrate') ?>">SFTP Password</a></strong>.
+		</li>
+	</ol>
 <?php
 } else {
 	// PUT FORM HERE
@@ -182,132 +175,4 @@ As clear from the picture above, password is same as your Pantheon dashboard pas
 <?php
 }
 	}
-endif;
-
-if ( !function_exists('bvKeyConf') ) :
-	function bvKeyConf() {
-		global $blogvault, $bvNotice;
-		$_error = NULL;
-		if (isset($_GET['error'])) {
-			$_error = $_GET['error'];
-		}
-?>
-
-
-<div class="bv_page_wide" style="display:block;background:#fff;padding-right:1%;overflow:hidden; margin-right:2.5%;margin-top:1%;"> <!-- SOWP MAIN -->
-
-	<div class="bv_inside_heading" style="padding:0.25% 0 0 2%;overflow:hidden;border-bottom:1px solid #ebebeb;">
-	<a href="http://blogvault.net/"><img src="<?php echo plugins_url('logo.png', __FILE__); ?>" /></a>
-	</div>
-
-
-	<div style="overflow:hidden;">	<!-- SOP 1 -->
-			<div class="bv_inside_column1" style="width:100%;max-width:90%;float:left;padding:1% 2.5% 1% 2.5%;border-right:1px solid #ebebeb;overflow:hidden;"> <!-- MCA -->
-<?php if (!isset($_REQUEST['free'])) { ?>
-						<div align="center" style="margin-bottom: 25px;">
-									<iframe style="border: 1px solid gray; padding: 3px;" src="https://player.vimeo.com/video/88638675?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff" width="450" height="275" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
-					</div>
-<?php } ?>
-
-
-<?php
-		echo $bvNotice;
-		if ($blogvault->getOption('bvPublic')) {
-?>
-		<div style="display:table;table-layout:fixed;width:100%;float:left;padding:1% 2.5% 2em 2.5%;overflow:hidden;" id="form_wrapper">
-				<font size='3'><a href='https://webapp.blogvault.net' target="_blank">Click here</a> to manage your backups from the <a href='https://webapp.blogvault.net' target="_blank">blogVault Dashboard.</a></font>
-				<br/><br/>
-<?php if (isset($_REQUEST['changekey'])) { ?>
-				<form method='post'>
-					<font size='3'>Change blogVault Key:</font> <input type='text' name='blogvaultkey' size='65'>
-					<input type='hidden' name='change_parameter' value='true'>
-					<input type='hidden' name='bvnonce' value='<?php echo wp_create_nonce("bvnonce") ?>'>
-					<input type='submit' value='Change'>
-				</form>
-<?php } ?>
-		</div>
-<?php } else { ?> <!-- Change keys ELSE -->
-			<div style="display:none">
-				<a href='http://blogvault.net?bvsrc=bvplugin&wpurl=<?php echo urlencode($blogvault->wpurl()) ?>'> Click here </a> to get your blogVault Key.</font>
-				<form method='post'> 
-					<font size='3'>Enter blogVault Key:</font> <input type='text' name='blogvaultkey' size='65'>
-					<input type='hidden' name='bvnonce' value='<?php echo wp_create_nonce("bvnonce") ?>'>
-					<input type='submit' value='Activate'>	
-				</form>
-			</div>
-<!-- form wrapper starts here-->
-<div style="display:table;table-layout:fixed;width:100%;max-width:40%;float:left;padding:1% 2.5% 2em 2.5%;overflow:hidden;border: 1px solid #ebebeb;" id="form_wrapper">
-<?php if (true) { ?>
-			<!-- Signin form end here -->
-			<div>
-				<font size="3">Login to your blogVault Account!</font><br/>
-				<font size="2">Learn more about blogVault and create your account <a href="https://blogvault.net">here</a></font>
-			</div>
-			<form rel="canonical" action="https://webapp.blogvault.net/home/api_signin" style="padding:0 2% 2em 1%;" method="post" name="signin">
-				<input type="hidden" name="bvsrc" value="wpplugin" />
-				<input type="hidden" name="url" value="<?php echo $blogvault->wpurl(); ?>">
-				<input type="hidden" name="secret" value="<?php echo $blogvault->getOption('bvSecretKey'); ?>">
-				<input type='hidden' name='bvnonce' value='<?php echo wp_create_nonce("bvnonce") ?>'>
-<?php if ($_error == "user") { ?>
-				<div style="color:red; font-weight: bold;">Incorrect Username or Password</div>
-<?php } ?>
-				<table style="border-spacing:50px 10px;">
-					<tr>
-						<td><label><strong>Email</strong></label></td>
-						<td><input type="text" name="email" /></td>
-					</tr>
-					<tr>
-						<td width="115"><label><strong>Password</strong></label></td>
-						<td><input type="password" name="password" /></td>
-					<tr/>
-					<?php if ($_error == "pass") echo '<tr><td colspan=3><p style="color:red;">The Email or password provided is incorrect</p></td></tr>' ?>
-					<tr>
-						<td></td>
-						<td align="right"><button type="submit">Sign In</button></td>
-					</tr>
-					<tr>
-						<td></td>
-						<td align="right"><a href="https://webapp.blogvault.net/password_resets/new?bvsrc=wpplugin&wpurl=<?php echo urlencode($blogvault->wpurl()) ?>" target="_blank">Forgot Password</a></td>
-					</tr>
-				</table>
-			</form>
-
-<?php } ?>
-		</div>	<!-- Signin	form ends here -->
-		<div class="bv_3part_column1" style="width:100%;max-width:45%;float:left;padding:3% 2.5% 0 2.5%;overflow:hidden;">
-					<div style="width:100%;overflow:hidden; margin-bottom: 10px;">
-								<blockquote><span class="bqstart" style="float:left;font-size:400%;color:#cfcfcf;">&#8220;</span><h2>blogVault is my favorite way to backup, migrate, and restore WordPress websites.&nbsp;&nbsp;<font size='2'><a href="http://bit.ly/mightyreview" style="text-decoration:none;" align="right" target="_blank">Read the complete review.</a></font></h2> <span style="float:right;"> - Kristin &#38; Mickey &#64; <a href="http://www.mightyminnow.com" style="text-decoration:none;" target="_blank">MIGHTYminnow</a> <font size='1'>(A Top WordPress Agency)</font></span></blockquote>
-					</div>
-				<font size='2' color="gray">As seen on:</font>
-				<div align="center" style="padding-top:3%;"><img src="<?php echo plugins_url('as_seen_in.png', __FILE__); ?>" /></div>
-		</div>
-
-	<?php
-	}
-?>
-			</div> <!-- MCA -->
-			<div class="bv_ selectedinside_column2" style="margin-top:0.5%;margin-right:0;border:0;max-width:19%;padding:0.5% 0 2em 1%;overflow:hidden;" align="center">
-				<!-- SIDE COLUMN CONTENT GOES HERE -->
-			</div>
-	</div> <!-- EOP 1 -->
-
-</div> <!-- EOWP MAIN -->
-<?php
-}
-endif;
-
-if ( !function_exists('bvActivateWarning') ) :
-	function bvActivateWarning() {
-		global $hook_suffix;
-		global $blogvault;
-		if (!$blogvault->getOption('bvPublic') && $hook_suffix == 'admin.php' ) {
-?>
-			<div id="message" class="updated" style="padding: 8px; font-size: 16px; background-color: #dff0d8">
-						<a class="button-primary" href="<?php echo admin_url('admin.php?page=bv-pantheon-key-config') ?>">Activate blogVault</a>	
-						&nbsp;&nbsp;&nbsp;<b>Almost Done:</b> Activate your blogVault account to backup your site.
-			</div>
-<?php
-		}
-	}
-	add_action('admin_notices', 'bvActivateWarning');
 endif;
